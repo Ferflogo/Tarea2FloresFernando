3ffb846d619051d53c1f4059a55c15867a290683
Ferflogo <55251038+Ferflogo@users.noreply.github.com>
2019-09-12 22:56:43 -0600

    Tarea_2f8_arreglado.py
    Se realizan los arreglos indicados con flake8

--- Tarea_2f8_errores.py
+++ Tarea_2f8_errores.py
@@ -1,11 +1,10 @@
 # Funcion con tres Errores para Flake8
 
-x = 0 
+x = 0
 
-
-Funcion para contar hasta 10
-for x in range (10):
-    x = x+1;
+# Funcion para contar hasta 10
+for x in range(10):
+    x = x+1
     print(x)
 
 print("Final")
